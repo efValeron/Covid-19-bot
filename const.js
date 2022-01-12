27c8f0f8338fa392041fad5c06d3db9a4200dff7
efValeron <masterbox06@gmail.com>
2022-01-12 22:07:39 +0300

    first commit

---
+++ const.js
@@ -0,0 +1,21 @@
+const commands = `
+/start - Перезапустить бота
+/help - Доступный команды
+`
+
+const msg_btn_1 = `
+Текст для проверки обработчика кнопки 1 и <a href="https://youtube.com/">ссылка</a>
+`
+
+const msg_btn_2 = `
+Текст для проверки обработчика кнопки 2 и <a href="https://store.steampowered.com/app/231430/Company_of_Heroes_2/">ссылка</a>
+`
+
+const msg_btn_3 = `
+JavaScript - мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили. Является реализацией спецификации ECMAScript.
+`
+
+module.exports.commands = commands
+module.exports.msg_btn_1 = msg_btn_1
+module.exports.msg_btn_2 = msg_btn_2
+module.exports.msg_btn_3 = msg_btn_3
---
+++ index.js
@@ -0,0 +1,50 @@
+const {Telegraf, Markup} = require('telegraf')
+require('dotenv').config()
+
+const imp = require('./const')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply(`Приветствую ${ctx.message.from.first_name ? ctx.message.from.first_name : 'странник'}!`))
+bot.help((ctx) => ctx.reply(imp.commands))
+
+bot.command('course', async (ctx) => {
+    try {
+        await ctx.replyWithHTML('<b>Курсы</b>', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Youtube', 'btn_1'), Markup.button.callback('CoH2', 'btn_2'), Markup.button.callback('JS info', 'btn_3')]
+            ]
+        ))
+    } catch (e) {
+        await ctx.reply('ERROR')
+        await ctx.reply(e)
+    }
+})
+
+function addActionBot(name, src, msg) {
+    bot.action(name, async (ctx) => {
+        try {
+            await ctx.answerCbQuery()
+            if (src !== false) {
+                await ctx.replyWithPhoto({
+                    source: src
+                })
+            }
+            await ctx.replyWithHTML(msg, {
+                disable_web_page_preview: true
+            })
+        } catch (e) {
+            await ctx.reply('ERROR')
+            await ctx.reply(e)
+        }
+    })
+}
+
+addActionBot('btn_1', false, imp.msg_btn_1)
+addActionBot('btn_2', './img/CoH2.png', imp.msg_btn_2)
+addActionBot('btn_3', false, imp.msg_btn_3)
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
